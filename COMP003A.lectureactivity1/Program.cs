1e919fae8af1307f9ace613468cbe0cf907a545c
Kenneth Schulz <kennethschulz@my.whccd.edu>
2025-01-15 20:45:41 -0800

    I wrote 10 facts about myself

--- COMP003A.lectureactivity1/Program.cs
+++ COMP003A.lectureactivity1/Program.cs
@@ -12,7 +12,21 @@ namespace COMP003A.lectureactivity1
         // main level
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+       
+            Console.WriteLine("Hi! \n my name is Kenneth Schulz");
+            Console.WriteLine("Here are my 10 things about me");
+            Console.WriteLine("\t 1. I am a math major");
+            Console.WriteLine("\t 2. I am a college athlete");
+            Console.WriteLine("\t 3. I like pizza");
+            Console.WriteLine("\t 4. I have a cat");
+            Console.WriteLine("\t 5. I have one brother");
+            Console.WriteLine("\t 6. I am a single man");
+            Console.WriteLine("\t 7. I am 19 years old");
+            Console.WriteLine("\t 8. I am going for a master degree");
+            Console.WriteLine("\t 9. I have one dog");
+            Console.WriteLine("\t 10. I am a religous man");
+         
+
         }
     }
 }
